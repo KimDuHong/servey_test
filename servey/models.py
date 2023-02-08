8d029ce63dcd1ff506c09613d9359a533900c71b
izunaaaaa <art970@naver.com>
2023-02-08 17:40:42 +0900

    servey post add

--- servey/models.py
+++ servey/models.py
@@ -3,16 +3,12 @@ from common.models import CommonModel
 
 # Create your models here.
 class servey(CommonModel):
-    title = models.CharField(
-        max_length=100,
-    )
+    title = models.TextField(max_length=100)
     first_answer = models.TextField(max_length=100)
     second_answer = models.TextField(max_length=100)
-    image = models.ImageField(
-        null=True,
-        blank=True,
-    )
     branch = models.CharField(max_length=4)
+    first_count = models.PositiveIntegerField()
+    second_count = models.PositiveIntegerField()
 
     def __str__(self) -> str:
         return self.title
