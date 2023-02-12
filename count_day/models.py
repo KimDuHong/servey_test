3f16981eb110363fd700794f04643c8508ba0257
izunaaaaa <art970@naver.com>
2023-02-12 22:20:11 +0900

    이용자 수 그래프 추가

--- count_day/models.py
+++ count_day/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from common.models import CommonModel
 
 # Create your models here.
+
+
 class count_day(CommonModel):
     day = models.DateField(unique=True)
     count = models.PositiveIntegerField()
--- servey/admin.py
+++ servey/admin.py
@@ -2,6 +2,20 @@ from django.contrib import admin
 from .models import servey
 
 # Register your models here.
+
+
+@admin.action(description="set all count Zero")
+def reset_all(model_admin, request, results):
+    for result in results:
+        result.first_count, result.second_count = 0, 0
+        result.save()
+
+
 @admin.register(servey)
 class ServeyAdmin(admin.ModelAdmin):
-    list_display = ("title",)
+    actions = (reset_all,)
+    list_display = (
+        "title",
+        "first_count",
+        "second_count",
+    )
